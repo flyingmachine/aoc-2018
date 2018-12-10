5794d348d0c2acdb666cec721733a01f6985119b
Daniel Higginbotham <daniel_higginbotham@apple.com>
2018-12-10 16:35:46 -0500

    some aoc solutions

--- test/aoc_2018/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns aoc-2018.core-test
-  (:require [clojure.test :refer :all]
-            [aoc-2018.core :refer :all]))
-
-
-(deftest a-test
-  (testing "FIXME, I don't fail."
-    (is (true? (= 4 (count (str (= 1 1))))))))
